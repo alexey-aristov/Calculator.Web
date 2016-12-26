e3a685583f4a6848335e39d1b4e1580395d304dd
alexey-aristov <alexey0206@gmail.com>
2016-12-25 22:55:54 -0800

    Initial draft

---
+++ script/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    var throttled = _.throttle(onScroll, 200);
+    $(document).on("scroll", throttled);
+    var inAction = false;
+
+    function onScroll(event) {
+        if (inAction)
+            return;
+        var screenHeight = $(".bearing_height").height();
+        var pos = document.body.scrollTop;
+
+        var o = document.body.scrollTop % screenHeight;
+        if (o < 100) {
+            var newPos = document.body.scrollTop - o;
+            document.body.scrollTop = newPos;
+            console.log("onScroll: from" + pos + "to:" + newPos);
+            return;
+        }
+        if (screenHeight - o < 100) {
+            var newPos = document.body.scrollTop + (screenHeight - o);
+            document.body.scrollTop = newPos;
+            console.log("onScroll: from" + pos + "to:" + newPos);
+            return;
+        }
+    }
+
+    $('#home').bind('mousewheel', function (event) {
+        event.preventDefault();
+        if (inAction)
+            return;
+        var deltaY = event.originalEvent.deltaY;
+        var step = $(".bearing_height").height() / 3;
+        var pos = document.body.scrollTop;
+        var nextPos = pos + (step * Math.sign(deltaY));
+
+        if (Math.abs(pos - nextPos) > 3) {
+            inAction = true;
+            $('html, body').animate({
+                scrollTop: nextPos
+            }, 50, function () {
+                inAction = false;
+            });
+            console.log("mousewheel: from" + pos + "to:" + nextPos + ", Step: " + step);
+        }
+    });
+});
+
